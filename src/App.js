97fdd81e05bc6f2c91a3067c6b07a8546c80909b
GunaSeelan23 <gunaseelan231299@gmail.com>
2023-09-19 22:11:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+const steps = [
+	{
+		id: '0',
+		message: 'Hiii . . .',
+		trigger: '1',
+	}, {
+		id: '1',
+		message: 'Please Enter your name',
+		trigger: '2'
+	}, {
+		id: '2',
+		user: true,
+		trigger: '3',
+	}, {
+		id: '3',
+		message: " Hi {previousValue}",
+		trigger: 4
+	}, {
+		id: '4',
+		options: [
+			{ value: 1, label: 'Have a Great Day' },
+			{ value: 2, label: 'Friend' },
+
+		],
+		end: true
+	}
+];
+
+const theme = {
+	background: '#ed8e7b',
+	headerBgColor: '#e81f10',
+	headerFontSize: '20px',
+	botBubbleColor: '#e81f10',
+	headerFontColor: 'Black',
+	botFontColor: 'white',
+	userBubbleColor: '#e8104a',
+	userFontColor: 'white',
+};
+
+const config = {
+	botAvatar: "img.png",
+	floating: true,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ThemeProvider theme={theme}>
+				<ChatBot
+					headerTitle="Chat Box"
+					steps={steps}
+					{...config}
+
+				/>
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
